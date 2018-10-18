1fd637c98214b524b29fcd8af7dddbb8107616a5
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2018-10-18 20:39:55 -0300

    terminando login

--- app/src/main/java/com/whatsapp/jmdevelopers/whatsappclone/LoginActivity.java
+++ app/src/main/java/com/whatsapp/jmdevelopers/whatsappclone/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.whatsapp.jmdevelopers.whatsappclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void abrirtelacadastro(View v){
+        Intent i = new Intent(LoginActivity.this,CadasroActivity.class);
+        startActivity(i);
+
+
     }
 }
