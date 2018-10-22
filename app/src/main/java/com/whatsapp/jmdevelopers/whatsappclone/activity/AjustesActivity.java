46450e09e5f772ed538b9f8c80c76c596a17942b
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2018-10-22 19:57:30 -0300

    Permissoes configuradas atualizado

--- app/src/main/java/com/whatsapp/jmdevelopers/whatsappclone/activity/AjustesActivity.java
+++ app/src/main/java/com/whatsapp/jmdevelopers/whatsappclone/activity/AjustesActivity.java
@@ -1,6 +1,10 @@
 package com.whatsapp.jmdevelopers.whatsappclone.activity;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -32,6 +36,36 @@ public class AjustesActivity extends AppCompatActivity {
         // SOLIICITAR PERMISSOES
         Permissao.validarpermissoes(permissoes,this,1);
 
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(int permissaoresultado:grantResults){
+            // verifica se foi negado
+            if(permissaoresultado==PackageManager.PERMISSION_DENIED){
+                alertapermissao();
+                /// avisa ao usuario
+
+            }
+
+        }
+    }
+    public void alertapermissao(){
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para ultilizar o App, é nescessario aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog dialog= builder.create();
+        dialog.show();
 
 
     }
--- app/src/main/java/com/whatsapp/jmdevelopers/whatsappclone/helper/Permissao.java
+++ app/src/main/java/com/whatsapp/jmdevelopers/whatsappclone/helper/Permissao.java
@@ -12,29 +12,24 @@ import java.util.List;
 public class Permissao {
     public static Boolean validarpermissoes(String[] permissoes, Activity activity,int requescode){
         // verificar se ele ta usando versao maior que a masshmalow
-        if(Build.VERSION.SDK_INT>=23){
-            List<String>  listadepermissoes=new ArrayList<>();
+        if(Build.VERSION.SDK_INT>=23) {
+            List<String> listadepermissoes = new ArrayList<>();
             // percorrer se as permissoes ja foram acionadas
-        for(String permissao:permissoes){
-          boolean tempermissao = ContextCompat.checkSelfPermission(activity,permissao) == PackageManager.PERMISSION_GRANTED;
-          if(!tempermissao){
-              listadepermissoes.add(permissao);
-          }
-          if(listadepermissoes.isEmpty()){
-              return true;
+            for (String permissao : permissoes) {
+                boolean tempermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+                if (!tempermissao) listadepermissoes.add(permissao);
+            }
 
-          }
+
+          if(listadepermissoes.isEmpty())return true;
+
           String[]novaspermissoes=new String[listadepermissoes.size()];
           listadepermissoes.toArray(novaspermissoes);
           // solificar permissao/
             ActivityCompat.requestPermissions(activity,novaspermissoes,requescode);
 
-
-
         }
 
-
-        }
         return true;
 
     }
