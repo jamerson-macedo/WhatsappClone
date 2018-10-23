54a0ea8cbc8119db882d0796584a7a3dccaf753e
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2018-10-22 21:55:36 -0300

    pegando foto da camera e galeria ok

--- app/src/main/java/com/whatsapp/jmdevelopers/whatsappclone/activity/AjustesActivity.java
+++ app/src/main/java/com/whatsapp/jmdevelopers/whatsappclone/activity/AjustesActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,11 +21,13 @@ import com.whatsapp.jmdevelopers.whatsappclone.helper.Permissao;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class AjustesActivity extends AppCompatActivity {
     private static final int SELECAO_CAMERA = 100;
     private static final int SELECAO_GALERIA = 200;
 
-
+    private CircleImageView imagem_perfil;
     private ImageButton camera;
     private ImageButton galeria;
     private String[] permissoes = new String[]{
@@ -37,6 +42,7 @@ public class AjustesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ajustes);
         camera = findViewById(R.id.imagebuttoncamera);
         galeria = findViewById(R.id.imagebuttongaleria);
+        imagem_perfil=findViewById(R.id.profile_image);
 
 
         Toolbar toolbar = findViewById(R.id.toolbarprincipal);
@@ -54,7 +60,7 @@ public class AjustesActivity extends AppCompatActivity {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 // STARTA ACTIVITY MAS PRECISA DO RETORNO
                 // testAR SE AINTENTE CONSEGUIU ABRIR
-                if (intent.resolveActivity(getPackageManager())!=null) {
+                if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(intent, SELECAO_CAMERA);
                 }
             }
@@ -64,10 +70,10 @@ public class AjustesActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // PEGA DA GALERIA
-                Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 // STARTA ACTIVITY MAS PRECISA DO RETORNO
                 // testAR SE AINTENTE CONSEGUIU ABRIR
-                if (intent.resolveActivity(getPackageManager())!=null) {
+                if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(intent, SELECAO_GALERIA);
                 }
 
@@ -77,6 +83,42 @@ public class AjustesActivity extends AppCompatActivity {
 
     }
 
+    // metodo para pegar a foto
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+// verifica se pegou
+        if ( resultCode == RESULT_OK ){
+            Bitmap imagem = null;
+
+            try {
+
+                switch ( requestCode ){
+                    // caso for a camera
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada );
+                        break;
+                }
+
+                if ( imagem != null ){
+
+                    imagem_perfil.setImageBitmap( imagem );
+
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    // metodos para permissoes
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
